fb84d3223c868ebf4f78bdc7e0a3452f45629563
Chloe L <cyleung284@gmail.com>
2019-01-03 18:47:25 -0600

    <auton draft 1- added TODOS, modifications>

--- src/ExtendedMath.java
+++ src/ExtendedMath.java
@@ -4,6 +4,16 @@ public class ExtendedMath {
         // should take in any number of radians, and output the equivalent in the range [-pi, pi]
         // do NOT use a loop.
         // hint: maybe make another function called normalizeRadians2pi which normalizes to [0, 2pi] first
-        return 0;
+        return (normalizeRad2pi(radians % (float)(2 * Math.PI)));
+    }
+
+    public static float normalizeRad2pi(float rad){
+        float result = rad;
+        //only normalizes if radians > pi
+        if(rad > Math.PI){
+            //pi = -pi ; 3pi/2 = -pi/2; 2pi = 0
+            result = (float)(rad - (2 * Math.PI));
+        }
+        return result;
     }
 }
--- src/Robot.java
+++ src/Robot.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 public class Robot {
     // assume that these are set/nonnull
     private Drivebase drivebase;
@@ -11,5 +13,9 @@ public class Robot {
     public void moveTo(Vector2F targetPosition, float targetHeading) {
         // move to the given position and heading
         // you should only have to use things that are within these files
+        Vector2F pos = navigationalState.getPosition();
+        Vector2F newpos = targetPosition.subtract(pos);
+        drivebase.rotate(newpos.getAngle());
+        drivebase.forwardDrive(newpos.getMagnitude());
     }
 }
